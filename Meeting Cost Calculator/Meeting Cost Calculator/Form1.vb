09eaac25e580679ef3e5625d128be98a4ad79418
Codered741 <Codered741@gmail.com>
2018-07-29 00:00:40 -0400

    added automatic calculation as inputs are changed

--- Meeting Cost Calculator/Meeting Cost Calculator/Form1.vb
+++ Meeting Cost Calculator/Meeting Cost Calculator/Form1.vb
@@ -11,19 +11,20 @@ Public Class Form1
 
         Dim versionNumber As Version
         versionNumber = Assembly.GetExecutingAssembly().GetName().Version
-        lblVer.Text = versionNumber.ToString()
+        lblVer.Text = "v" & versionNumber.ToString()
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    Public Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         SetEnables()
 
         Dim elapsed As TimeSpan = Me.stopwatch.Elapsed
         lblET.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}",
             Math.Floor(elapsed.TotalHours), elapsed.Minutes, elapsed.Seconds, elapsed.Milliseconds)
         'ElapsedTime*HourlyRate*#OfAttendees
-        lblMC.Text = "$" & Math.Round(elapsed.TotalSeconds * (Integer.Parse(tbRate.Text) / 3600) * Integer.Parse(tbNumAtt.Text), 2)
+        lblMC.Text = Calculate()
+        '"$" & Math.Round(elapsed.TotalSeconds * (Integer.Parse(tbRate.Text) / 3600) * Integer.Parse(tbNumAtt.Text), 2)
     End Sub
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
@@ -49,6 +50,8 @@ Public Class Form1
         If Not Integer.TryParse(tbRate.Text, intRate) Then
             MsgBox("Please enter a whole number as the hourly rate.  ")
             tbRate.Text = 170
+        Else
+            lblMC.Text = Calculate()
         End If
 
     End Sub
@@ -59,7 +62,10 @@ Public Class Form1
         If Not Integer.TryParse(tbNumAtt.Text, intNumAtt) Then
             MsgBox("Please enter a whole number for the # of attendees.  ")
             tbNumAtt.Text = 1
+        Else
+            lblMC.Text = Calculate()
         End If
+
     End Sub
 
     Private Sub SetEnables()
@@ -78,4 +84,8 @@ Public Class Form1
         End If
     End Sub
 
+    Private Function Calculate() As String
+        Dim elapsed As TimeSpan = Me.stopwatch.Elapsed
+        Calculate = "$" & Math.Round(elapsed.TotalSeconds * (Integer.Parse(tbRate.Text) / 3600) * Integer.Parse(tbNumAtt.Text), 2)
+    End Function
 End Class
